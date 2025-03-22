bd29e9945481cae4002cb1c5d592c50db4103bef
WebStudy <americanian@naver.com>
2025-03-22 16:15:13 +0900

    추가로 자바 문법들 공부 2

---
+++ [Java] enumType 예제 , 예외 처리 예제 , generic 예제 , immutable 객체 예제 , wrapper 예제 ,추가로 자바 문법들 공부 2/241020/src/java_1020/A4.java
@@ -0,0 +1,19 @@
+package java_1020;
+
+interface Car{}
+
+class Sonata implements Car {}
+
+class K5 implements Car {}
+
+public class A4 {
+
+	public static void main(String[] args) {
+		Car car = new K5(); 
+		Car car1 = new Sonata(); 
+			
+		}
+
+}
+
+
---
+++ [Java] enumType 예제 , 예외 처리 예제 , generic 예제 , immutable 객체 예제 , wrapper 예제 ,추가로 자바 문법들 공부 2/241020/src/java_1020/A9.java
@@ -0,0 +1,26 @@
+package java_1020;
+
+
+public class A9{
+	
+	public static void main(String[] args) {
+		String isThisNull = getSomeString();
+		//System.out.println(isThisNull.toUpperCase());
+		
+		if(isThisNull != null) {
+			System.out.println(isThisNull.toUpperCase());
+		}else {
+			System.out.println("null");
+		}
+	
+	}
+	
+	private static String getSomeString() {
+		return null; // 항상 null 반환
+	}
+	
+}
+
+
+
+
